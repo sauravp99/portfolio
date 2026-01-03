3a64d7e44339ad87ad0f3a5b3ec5cba9059024e9
sauravp99 <saurav.pa99@gmail.com>
2026-01-03 12:17:46 +0100

    add general layout

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,20 @@ export const Route = createFileRoute("/")({
 });
 
 function Index() {
-  return <div className="bg-red-100 w-full h-full">Text here</div>;
+  return (
+    <main className="bg-red-100 w-full h-screen grid grid-cols-12 *:border-2">
+      <div className="col-span-1">side label</div>
+      <section className="col-span-10 flex flex-col items-center">
+        <header>Saurav Pandey</header>
+        <div className="flex flex-col">
+          <div className="">Picture of me</div>
+          <div className="">Info of me</div>
+        </div>
+      </section>
+      <div className="col-span-1">side label</div>
+      <section>
+        <div>my work</div>
+      </section>
+    </main>
+  );
 }
