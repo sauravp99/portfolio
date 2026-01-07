9d40d00faf2babef77648cba5887c3ea058e0b65
sauravp99 <saurav.pa99@gmail.com>
2026-01-08 00:51:38 +0100

    add gifs and import projectCard components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,11 @@ import { createFileRoute } from "@tanstack/react-router";
 import { Header } from "../components/home/Header";
 import { WelcomeBanner } from "../components/home/WelcomeBanner";
 import { PortraitSection } from "../components/home/PortraitSection /PortraitSection";
+import prosperGif from "../assets/Text generation loop.json";
+import aiGif from "../assets/AIGif.json";
+import windmillGif from "../assets/Wind Mill.json";
+import Lottie from "lottie-react";
+import { ProjectCard } from "../components/home/ProjectCard/ProjectCard";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -16,7 +21,7 @@ function Index() {
         </section>
         <section className="col-span-10 flex flex-col items-center ">
           <Header title={"SAURAV PANDEY"} />
-          <div className="lg:pt-80 md:pt-30 sm:pt-50 pt-30 w-full">
+          <div className="lg:pt-40 md:pt-30 sm:pt-50 pt-30 w-full">
             <PortraitSection />
           </div>
         </section>
@@ -24,7 +29,34 @@ function Index() {
           <WelcomeBanner side="right" />
         </section>
       </section>
-      {/* Cards flying in here, add hover effect on cards */}
+      <div className="grid grid-cols-12 pt-50">
+        <div className="col-span-1" />
+        <div className="flex flex-row col-span-10 justify-between w-full *:rounded-xl *:w-1/4">
+          <ProjectCard
+            title="Prosper AI"
+            year="2023 -> 2024"
+            description="Automating property prospect generation with AI"
+            animationDelay={20}
+            gif={<Lottie animationData={prosperGif} loop={true} />}
+          />
+          <ProjectCard
+            title="AI task force"
+            year="2023 -> 2024"
+            animationDelay={30}
+            description="A research and discovery driven team focused on exploring the capabilites of AI"
+            gif={<Lottie animationData={aiGif} loop={true} />}
+          />
+          <ProjectCard
+            title="Save my city!"
+            year="2022 -> 2023"
+            animationDelay={40}
+            description="An interactive serious game for engaging adolescents on
+              environmental studies"
+            gif={<Lottie animationData={windmillGif} loop={true} />}
+          />
+        </div>
+        <div className="col-span-1" />
+      </div>
       <section className="h-200">ds</section>
     </main>
   );
