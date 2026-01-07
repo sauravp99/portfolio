568e1ee91aafe28585c165bba2a64e7a3b6339bf
sauravp99 <saurav.pa99@gmail.com>
2026-01-08 00:56:34 +0100

    styling tweak on header

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -6,7 +6,7 @@ import PencilRulerIcon from "../../icons/pencil_ruler.svg";
 
 export const Header = ({ title }: { title: string }) => {
   return (
-    <div className=" w-full flex flex-col md:gap-20 gap-10 col-span-1 md:pt-40 pt-50 items-center justify-center">
+    <div className=" w-full flex flex-col md:gap-20 gap-10 col-span-1 md:pt-60 pt-50 items-center justify-center">
       {/* TODO: Use vite-svgr for svgs */}
       <div className="flex flex-row gap-10 md:gap-20 *:w-5 *:xl:w-10 *:lg:w-8 *:md:w-7">
         <img src={CodeIcon} alt="Code Icon" />
