41b0e88e0d159bdb0fc7a202f12fafc6b5e2ea9b
sauravp99 <saurav.pa99@gmail.com>
2026-01-09 22:12:19 +0100

    add content for ai task force

--- src/routes/work/aitaskforce.tsx
+++ src/routes/work/aitaskforce.tsx
@@ -4,6 +4,8 @@ import CodeIcon from "../../icons/code.svg";
 import GlassesIcon from "../../icons/glasses.svg";
 import HourglassIcon from "../../icons/hourglass.svg";
 import PencilRulerIcon from "../../icons/pencil_ruler.svg";
+import { ProjectImages } from "../../components/work/ProjectImages";
+import AiCover from "../../assets/aitaskforce/cover.png";
 
 export const Route = createFileRoute("/work/aitaskforce")({
   component: () => RouteComponent(),
@@ -11,10 +13,10 @@ export const Route = createFileRoute("/work/aitaskforce")({
 
 function RouteComponent() {
   return (
-    <main className="bg-(--saffron_red) min-h-screen text-(--sand) overflow-hidden">
+    <main className="bg-[#280042] min-h-screen  flex flex-col gap-40 text-(--sand) overflow-hidden">
       <ProjectHeader
-        projectTitle={"Tale til tekst"}
-        clientName={"Utlendingsdirektoratet (UDI)"}
+        projectTitle={"AI task force"}
+        clientName={"Computas AS (R&D team)"}
         icons={
           <div className="flex flex-row gap-10 md:gap-20 *:w-5 *:xl:w-10 *:lg:w-8 *:md:w-7">
             <img src={CodeIcon} alt="Code Icon" />
@@ -24,6 +26,13 @@ function RouteComponent() {
           </div>
         }
         year={"2023 -> 2024"}
+      />
+      <ProjectImages
+        imageL={{
+          src: AiCover,
+          alt: "Illustration of projects in ai task force",
+          className: " max-w-[70%]",
+        }}
       />
     </main>
   );
