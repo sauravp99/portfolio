05371492d80d220a823fd5596f6082aca820da70
sauravp99 <saurav.pa99@gmail.com>
2026-01-09 02:08:21 +0100

    add componet for project images

---
+++ src/components/work/ProjectImages.tsx
@@ -0,0 +1,35 @@
+import clsx from "clsx";
+
+interface ProjectImage {
+  src: string;
+  alt: string;
+  className?: string;
+}
+export const ProjectImages = ({
+  imageL,
+  imageR,
+}: {
+  imageL: ProjectImage;
+  imageR: ProjectImage;
+}) => {
+  return (
+    <section className="w-full px-10 flex gap-20 flex-row justify-center items-center">
+      <img
+        src={imageL.src}
+        alt={imageL.alt}
+        className={clsx(
+          "max-w-[20%] h-auto bg-[#3f627855] p-4 rounded-2xl",
+          imageL.className
+        )}
+      />
+      <img
+        src={imageR.src}
+        alt={imageR.alt}
+        className={clsx(
+          "max-w-[20%] h-auto bg-[#3f627855] p-4 rounded-2xl",
+          imageR.className
+        )}
+      />
+    </section>
+  );
+};
