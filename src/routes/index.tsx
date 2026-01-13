1d7595d1ec3583497559483f470f1eb1e50d98bb
sauravp99 <saurav.pa99@gmail.com>
2026-01-13 13:39:45 +0100

    display under maintainence screen on smaller screens

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,28 @@ import { PortraitSection } from "../components/home/AboutMeSection/AboutMe";
 import prosperGif from "../assets/prosper/textGif.json";
 import aiGif from "../assets/aitaskforce/aiGif.json";
 import windmillGif from "../assets/masters/windmillGif.json";
+import constructionGif from "../assets/construction.json";
 import Lottie from "lottie-react";
 import { ProjectCard } from "../components/home/ProjectSection/ProjectCard/ProjectCard";
+import { useMediaQuery } from "usehooks-ts";
 
 export const Route = createFileRoute("/")({
-  component: Index,
+  component: () => Index(),
 });
 
-function Index() {
+const Index = () => {
+  const isNarrowScreen = useMediaQuery("(max-width: 1050px)");
+
+  if (isNarrowScreen) {
+    return (
+      <div className="min-h-screen bg-[#2e0101] flex items-center justify-center flex-col w-full">
+        <Lottie animationData={constructionGif} loop={true} />{" "}
+        <h3 className="text-2xl w-full text-center text-white font-days-one">
+          Responsive design coming soon, stay tuned!
+        </h3>
+      </div>
+    );
+  }
   return (
     <main className="bg-(--saffron_red) min-h-screen text-(--sand) overflow-hidden">
       <section className="grid grid-cols-12 min-h-20 w-full">
@@ -33,6 +47,7 @@ function Index() {
         <div className="col-span-1" />
         <div className="flex flex-row col-span-10 justify-between w-full *:rounded-xl *:w-1/4">
           <ProjectCard
+            to="/work/prosperai"
             title="Prosper AI"
             year="2024 -> 2025"
             description="Automating property prospect generation with AI"
@@ -40,6 +55,7 @@ function Index() {
             gif={<Lottie animationData={prosperGif} loop={true} />}
           />
           <ProjectCard
+            to="/work/aitaskforce"
             title="AI task force"
             year="2023 -> 2024"
             animationDelay={30}
@@ -47,6 +63,7 @@ function Index() {
             gif={<Lottie animationData={aiGif} loop={true} />}
           />
           <ProjectCard
+            to="/work/savemycity"
             title="Save my city!"
             year="2022 -> 2023"
             animationDelay={40}
@@ -57,7 +74,6 @@ function Index() {
         </div>
         <div className="col-span-1" />
       </div>
-      <section className="h-200">ds</section>
     </main>
   );
-}
+};
