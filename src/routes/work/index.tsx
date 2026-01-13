61fbfca3cea4de0be412b3f65556036d7f2a3e0d
sauravp99 <saurav.pa99@gmail.com>
2026-01-13 10:44:13 +0100

    redirect to home

--- src/routes/work/index.tsx
+++ src/routes/work/index.tsx
@@ -1,9 +1,5 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/work/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/work/"!</div>
-}
+export const Route = createFileRoute("/work/")({
+  loader: () => redirect({ to: "/" }),
+});
